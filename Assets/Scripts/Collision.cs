fea3d76ebea52315bf40d21ca508e86361ec6d51
ITOTAKUMA <92348195+ITOTAKUMA@users.noreply.github.com>
2021-12-03 09:39:58 +0900

    Add files via upload

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    Hook m_Hook;
+
+
+    private void Start()
+    {
+        this.m_Hook = FindObjectOfType<Hook>();
+    }
+
+
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        if (collision.gameObject.tag == "Object")
+        {
+            //Debug.Log("‚Í‚¢");
+
+            m_Hook.m_AttractFlg = true;
+            m_Hook.m_HookShotFlg = false;
+        }
+
+        if(collision.gameObject.tag == "Enemy")
+        {
+            m_Hook.m_ComeEnemyFlg = true;
+            m_Hook.m_HookShotFlg = false;
+            m_Hook.m_HookReturnFlg = true;
+        }
+
+    }
+}
