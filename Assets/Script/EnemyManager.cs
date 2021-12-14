680360e5f34908f38d48698d8c348017b69efda5
ITOTAKUMA <92348195+ITOTAKUMA@users.noreply.github.com>
2021-12-14 11:47:55 +0900

    Add files via upload

---
+++ Assets/Script/EnemyManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public int count = 0;
+    float span = 5f;
+    public float time = 0f;
+    public GameObject EnemyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > span && count < 4)
+        {
+            GameObject enemy = (GameObject)Instantiate(EnemyPrefab, new Vector3(Random.Range(10, 30), 5, Random.Range(10, 30)), Quaternion.identity);
+
+            count++;
+            time = 0;
+        }
+
+    }
+}
+
+//public interface hidame
+//{
+//    void hidame_01(int damage);
+//}
+
+//public interface death
+//{
+//    void death_01();
+//}
+
+//public interface Move
+//{
+//    bool MoveEnemy(Vector3 _pos);
+//}
+
