14428420a1492e6ead6c4cad8174b94f945c29f1
Iwamoto-Kento <75952872+Iwamoto-Kento@users.noreply.github.com>
2022-01-12 11:04:18 +0900

    chang

--- Assets/Script/EnemyHp.cs
+++ Assets/Script/EnemyHp.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHp : MonoBehaviour, hidame, death
 {
@@ -8,10 +9,15 @@ public class EnemyHp : MonoBehaviour, hidame, death
     //hidame(自分で作ったインターフェースの名前ファイル名
     //これをやるとインターフェースの中のやつが使える
     public int hp = 10;                   //敵の体力
+    public Slider slider;
 
     void hidame.hidame_01(int damage)    //hidameというファイルの中の
     {                                  //hidame_01という機能を使うぞって意味です
         hp -= damage;
+        if (slider.value > 0)
+        {
+            slider.value = hp;
+        }
     }
 
     void death.death_01()
@@ -22,7 +28,7 @@ public class EnemyHp : MonoBehaviour, hidame, death
     // Use this for initialization
     void Start()
     {
-
+        slider.value = hp;
     }
     // Update is called once per frame
     void Update()
